67c6e368931e95756ec460a397e0f3ab4cc5e13c
Dmitry Kopievsky <193384474+kpvskd@users.noreply.github.com>
2025-01-10 16:28:55 +0200

    Add hammerspoon configuration

---
+++ hammerspoon/.config/hammerspoon/init.lua
@@ -0,0 +1,10 @@
+---@diagnostic disable: undefined-global
+
+-- Settings
+hs.autoLaunch(true)
+hs.automaticallyCheckForUpdates(false)
+hs.consoleOnTop(false)
+hs.dockIcon(false)
+hs.menuIcon(false)
+hs.uploadCrashData(false)
+hs.window.animationDuration = 0
