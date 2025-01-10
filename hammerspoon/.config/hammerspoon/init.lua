06ca148cfb9c9338f7cb469f9907f030a724eec3
Dmitry Kopievsky <193384474+kpvskd@users.noreply.github.com>
2025-01-10 16:37:14 +0200

    Add keybinding for Ctrl + hjkl to emulate Vim motion

--- hammerspoon/.config/hammerspoon/init.lua
+++ hammerspoon/.config/hammerspoon/init.lua
@@ -17,6 +17,16 @@ local function positionWindow(x, y, w, h)
 	end
 end
 
+local function pressFn(mods, key)
+	if key == nil then
+		key = mods
+		mods = {}
+	end
+	return function()
+		hs.eventtap.keyStroke(mods, key, 1000)
+	end
+end
+
 -- Settings
 hs.autoLaunch(true)
 hs.automaticallyCheckForUpdates(false)
@@ -40,3 +50,9 @@ hs.hotkey.bind(hyper, "c", function()
 	hs.window.focusedWindow():centerOnScreen(nil, true, 0)
 end)
 hs.hotkey.bind(hyper, "return", positionWindow(0, 0, 1, 1))
+
+-- Ctrl + hjkl to arrow keys
+hs.hotkey.bind("ctrl", "h", pressFn("left"), nil, pressFn("left"))
+hs.hotkey.bind("ctrl", "j", pressFn("down"), nil, pressFn("down"))
+hs.hotkey.bind("ctrl", "k", pressFn("up"), nil, pressFn("up"))
+hs.hotkey.bind("ctrl", "l", pressFn("right"), nil, pressFn("right"))
