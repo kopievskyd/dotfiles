51d4eee2a7fe650c3afa132a364ad64d0d886058
Dmitry Kopievsky <kopievskyd@gmail.com>
2025-02-16 20:39:20 +0200

    setup.sh: simplify symlink creation and remove unnecessary logic
    - Remove manual symlink creation logic
    - Replaced symlink creation with a single `stow` command to manage all dotfiles
    - Replaced the `brew bundle` path with a global `brew bundle --global` command for better compatibility
    - Removed redundant macOS default for disabling VSCode key repeat behavior

--- setup.sh
+++ setup.sh
@@ -1,15 +1,7 @@
 #!/bin/sh
 
 CONFIG_DIR="$(pwd)"
-COMPLETION_DIR="$CONFIG_DIR/zsh/completion"
-BREWFILE_PATH="$CONFIG_DIR/brew/Brewfile"
 FONT_DIR="$HOME/Library/Fonts"
-
-# Create symlink and its parent directory if needed
-symlink() {
-    mkdir -p "$(dirname "$2")"
-    ln -sf "$1" "$2" && echo "Created symlink: $1 -> $2" || echo "Failed to create symlink: $1 -> $2"
-}
 
 # Install Homebrew if not present
 install_homebrew() {
@@ -25,28 +17,14 @@ install_homebrew() {
 
 # Create all required symlinks
 create_symlinks() {
-    # Create symlinks for zsh completion files
-    for file in "$COMPLETION_DIR"/*; do
-        symlink "$file" "$HOME/.config/zsh/completion/$(basename "$file")"
-    done
-
-    # Create symlinks for config files
-    symlink "$CONFIG_DIR/zsh/.zshenv" "$HOME/.zshenv"
-    symlink "$CONFIG_DIR/zsh/.zshrc" "$HOME/.config/zsh/.zshrc"
-    symlink "$CONFIG_DIR/zsh/.p10k.zsh" "$HOME/.config/zsh/.p10k.zsh"
-    symlink "$CONFIG_DIR/git/config" "$HOME/.config/git/config"
-    symlink "$CONFIG_DIR/git/ignore" "$HOME/.config/git/ignore"
-    symlink "$CONFIG_DIR/vim/vimrc" "$HOME/.config/vim/vimrc"
-    symlink "$CONFIG_DIR/ghostty/config" "$HOME/.config/ghostty/config"
-    symlink "$CONFIG_DIR/hammerspoon/init.lua" "$HOME/.config/hammerspoon/init.lua"
-    symlink "$CONFIG_DIR/vscode/settings.json" "$HOME/Library/Application Support/Code/User/settings.json"
+    stow --no-folding --restow */
 }
 
 # Install packages from Brewfile
 install_brew_packages() {
     if [ -f "$BREWFILE_PATH" ]; then
         echo "Installing formulae and casks from Brewfile..."
-        brew bundle --file="$BREWFILE_PATH"
+        brew bundle --global
         brew cleanup --prune=all
         rm -rf "$(brew --cache)"
     else
@@ -107,7 +85,6 @@ install_jetbrains_mono() {
 # Macos defaults setup
 macos_defaults_setup() {
     defaults write org.hammerspoon.Hammerspoon MJConfigFile "$HOME/.config/hammerspoon/init.lua"
-    defaults write com.microsoft.VSCode ApplePressAndHoldEnabled -bool false
     defaults write com.apple.desktopservices DSDontWriteNetworkStores -bool true
     defaults write com.apple.desktopservices DSDontWriteUSBStores -bool true
     defaults write com.apple.dock autohide-delay -float 0 && \
