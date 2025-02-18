f0754871c1a3691fa20e19538b2ced98fc433c3c
Dmitry Kopievsky <kopievskyd@gmail.com>
2025-02-18 13:36:40 +0200

    p10k: unify prompt symbols across vi modes

--- zsh/.config/zsh/.p10k.zsh
+++ zsh/.config/zsh/.p10k.zsh
@@ -69,6 +69,12 @@
     typeset -g POWERLEVEL9K_PROMPT_CHAR_ERROR_{VIINS,VICMD,VIVIS}_FOREGROUND=red
     # Default prompt symbol.
     typeset -g POWERLEVEL9K_PROMPT_CHAR_{OK,ERROR}_VIINS_CONTENT_EXPANSION='%B➜'
+    # Prompt symbol in command vi mode.
+    typeset -g POWERLEVEL9K_PROMPT_CHAR_{OK,ERROR}_VICMD_CONTENT_EXPANSION='%B➜'
+    # Prompt symbol in visual vi mode is the same as in command mode.
+    typeset -g POWERLEVEL9K_PROMPT_CHAR_{OK,ERROR}_VIVIS_CONTENT_EXPANSION='%B➜'
+    # Prompt symbol in overwrite vi mode is the same as in command mode.
+    typeset -g POWERLEVEL9K_PROMPT_CHAR_OVERWRITE_STATE=false
 
     # Blue current directory.
     typeset -g POWERLEVEL9K_DIR_FOREGROUND=cyan
