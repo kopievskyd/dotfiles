c021103c5c8282c1c5cdaef87e5342ee15b0a792
Dmitry Kopievsky <kopievskyd@gmail.com>
2025-11-11 16:49:58 +0200

    chore: remove all existing files to rebuild configuration from scratch

--- nvim/plugin/mini_pairs.lua
+++
@@ -1,9 +0,0 @@
-local group = vim.api.nvim_create_augroup("Plugins", { clear = false })
-vim.api.nvim_create_autocmd("InsertEnter", {
-	group = group,
-	once = true,
-	callback = function()
-		vim.cmd.packadd("mini.pairs")
-		require("mini.pairs").setup()
-	end,
-})
--- nvim/plugin/surround.lua
+++
@@ -1,9 +0,0 @@
-local group = vim.api.nvim_create_augroup("Plugins", { clear = false })
-vim.api.nvim_create_autocmd("CursorMoved", {
-	group = group,
-	once = true,
-	callback = function()
-		vim.cmd.packadd("nvim-surround")
-		require("nvim-surround").setup()
-	end,
-})
