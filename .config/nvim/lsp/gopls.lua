ce0f9c80f935c143d186fac2cf019e8cb2fad68a
Dmitry Kopievsky <kopievskyd@gmail.com>
2025-12-26 23:34:01 +0200

    nvim: add gopls LSP configuration

---
+++ .config/nvim/lsp/gopls.lua
@@ -0,0 +1,11 @@
+return {
+	cmd = { "gopls" },
+	filetypes = { "go", "gomod", "gowork", "gotmpl", "gosum" },
+	root_markers = { "go.mod", "go.work", ".git" },
+	settings = {
+		gopls = {
+			analyses = { shadow = true },
+			staticcheck = true,
+		},
+	},
+}
