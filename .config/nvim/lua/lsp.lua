2c6637c21f7c4a1fb34d0580a5f973aa673e04d6
Dmitry Kopievsky <kopievskyd@gmail.com>
2026-01-10 17:10:45 +0200

    nvim: simplify LSP completion

--- .config/nvim/lua/lsp.lua
+++ .config/nvim/lua/lsp.lua
@@ -23,9 +23,6 @@ vim.lsp.config("*", {
 	on_attach = function(client, bufnr)
 		vim.lsp.completion.enable(true, client.id, bufnr, {
 			autotrigger = true,
-			convert = function(item)
-				return { abbr = item.label:gsub("%b()", "") }
-			end,
 		})
 	end,
 })
