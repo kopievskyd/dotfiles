7aadb3081e125274f59a8684a74b6bf8f202ad0e
Dmitry Kopievsky <kopievskyd@gmail.com>
2026-01-13 12:24:03 +0200

    nvim: simplify file explorer toggle

--- .config/nvim/lua/utils.lua
+++ .config/nvim/lua/utils.lua
@@ -1,40 +1,13 @@
 local M = {}
 
--- Toggle netrw file explorer
+-- Toggle file explorer
 function M.toggle_explore()
 	if vim.bo.filetype ~= "netrw" then
-		return vim.cmd("Explore")
-	end
-
-	local netrw_buf = vim.api.nvim_get_current_buf()
-
-	-- Try to switch to alternate buffer if it's valid and not netrw
-	local alt_buf = vim.fn.bufnr("#")
-	local switched = false
-
-	if
-		alt_buf > 0
-		and vim.api.nvim_buf_is_valid(alt_buf)
-		and vim.fn.buflisted(alt_buf) == 1
-		and vim.fn.getbufvar(alt_buf, "&filetype") ~= "netrw"
-	then
-		vim.cmd("buffer " .. alt_buf)
-		switched = true
-	end
-
-	-- If still in netrw, find first non-netrw buffer
-	if not switched then
-		for _, buf in ipairs(vim.fn.getbufinfo({ buflisted = 1 })) do
-			if vim.fn.getbufvar(buf.bufnr, "&filetype") ~= "netrw" then
-				vim.cmd("buffer " .. buf.bufnr)
-				break
-			end
-		end
-	end
-
-	-- Delete the netrw buffer
-	if vim.api.nvim_buf_is_valid(netrw_buf) then
-		vim.cmd("silent! bwipeout " .. netrw_buf)
+		vim.cmd.Explore()
+	elseif vim.fn.exists("w:netrw_rexlocal") == 1 then
+		vim.cmd.Rexplore()
+	else
+		vim.cmd("b#")
 	end
 end
 
