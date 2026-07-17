d473d2c583948e0ec2c186ccb19e81d6487cc794
Dmitry Kopievsky <kopievskyd@gmail.com>
2026-07-18 00:58:23 +0300

    zsh: improve plugin installation
    - clone plugins quietly
    - handle clone failures gracefully
    - add plug-update command

--- .config/zsh/functions/plug.zsh
+++ .config/zsh/functions/plug.zsh
@@ -1,18 +1,32 @@
 # Path for plugins
 readonly _PLUGINS_DIR="${XDG_DATA_HOME:-$HOME/.local/share}/zsh/plugins"
 
-# Source plugin file if exists
+# Source plugin file if it exists
 function _load_plugin() {
     local file="$_PLUGINS_DIR/$1"
-    [[ -f $file ]] && source "$file"
+    [[ -f "$file" ]] && source "$file"
 }
 
-# Public function to load plugin
+# Public function to load a plugin
 function plug() {
     local repo="$1"
     local name="${repo##*/}"
     local dir="$_PLUGINS_DIR/$name"
-    [[ -d $_PLUGINS_DIR ]] || mkdir -p "$_PLUGINS_DIR"
-    [[ -d $dir ]] || git clone --depth=1 "https://github.com/$repo.git" "$dir"
+
+    if [[ ! -d "$dir" ]]; then
+        mkdir -p "$_PLUGINS_DIR"
+        git clone --quiet --depth=1 "https://github.com/$repo.git" "$dir" || return 1
+    fi
+
     _load_plugin "$name/$name.plugin.zsh" || _load_plugin "$name/$name.zsh"
+}
+
+# Public function to update all installed plugins
+function plug-update() {
+    local dir
+    for dir in "$_PLUGINS_DIR"/*/; do
+        [[ -d "$dir/.git" ]] || continue
+        echo "Updating ${dir:t}..."
+        git -C "$dir" pull --quiet --ff-only
+    done
 }
