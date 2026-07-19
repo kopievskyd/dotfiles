f9f3cd59048ff4a2b3c2a7ccf53ae1e2f9c0e1dc
Dmitry Kopievsky <kopievskyd@gmail.com>
2026-07-19 12:53:14 +0300

    zsh: refactor plug function
    - validate plugin arguments
    - clean up failed plugin installations
    - isolate function execution
    - simplify plugin loading

--- .config/zsh/functions/plug.zsh
+++ .config/zsh/functions/plug.zsh
@@ -1,32 +1,44 @@
 # Path for plugins
 readonly _PLUGINS_DIR="${XDG_DATA_HOME:-$HOME/.local/share}/zsh/plugins"
 
-# Source plugin file if it exists
-function _load_plugin() {
-    local file="$_PLUGINS_DIR/$1"
-    [[ -f "$file" ]] && source "$file"
-}
-
 # Public function to load a plugin
 function plug() {
+    emulate -L zsh
+
+    (( $# == 1 )) || return 1
+
     local repo="$1"
     local name="${repo##*/}"
     local dir="$_PLUGINS_DIR/$name"
+    local file
 
     if [[ ! -d "$dir" ]]; then
         mkdir -p "$_PLUGINS_DIR"
-        git clone --quiet --depth=1 "https://github.com/$repo.git" "$dir" || return 1
+        git clone --quiet --depth=1 "https://github.com/$repo.git" "$dir" || {
+            rm -rf -- "$dir"
+            return 1
+        }
     fi
 
-    _load_plugin "$name/$name.plugin.zsh" || _load_plugin "$name/$name.zsh"
+    if [[ -f "$dir/$name.plugin.zsh" ]]; then
+        file="$dir/$name.plugin.zsh"
+    elif [[ -f "$dir/$name.zsh" ]]; then
+        file="$dir/$name.zsh"
+    else
+        return 1
+    fi
+
+    source "$file"
 }
 
 # Public function to update all installed plugins
-function plug-update() {
+function plug_update() {
+    emulate -L zsh
+
     local dir
-    for dir in "$_PLUGINS_DIR"/*/; do
+    for dir in "$_PLUGINS_DIR"/*(N/); do
         [[ -d "$dir/.git" ]] || continue
-        echo "Updating ${dir:t}..."
+        print "Updating ${dir:t}..."
         git -C "$dir" pull --quiet --ff-only
     done
 }
